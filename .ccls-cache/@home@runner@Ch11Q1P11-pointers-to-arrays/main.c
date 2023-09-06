be81203d247bc11f648fd6e0eb1099de772b35d9
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-06 12:28:10 +0000

    copied program

--- .ccls-cache/@home@runner@Ch11Q1P11-pointers-to-arrays/main.c
+++ .ccls-cache/@home@runner@Ch11Q1P11-pointers-to-arrays/main.c
@@ -1,6 +1,27 @@
+// Function to return the sum of the elements of an integer array
+
 #include <stdio.h>
 
+// Declare Functions
+int arraySum (int [], const int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  int values[10] = { 3, 7, -9, 3, 6, -1, 7, 9, 1, -5 };
+
+  printf ("The sum is %d\n", arraySum (values, 10));
+  
+  
   return 0;
+}
+
+// Define Functions
+int arraySum (int array[], const int n) {
+  int sum = 0, *ptr;
+  int *const arrayEnd = array + n;
+
+  for (ptr = array; ptr < arrayEnd; ++ptr)
+    sum += *ptr;
+
+  return sum;
 }
